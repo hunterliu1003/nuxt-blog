f25f0eeea0ffb9b38867d745430ec84baa785edc
Hunter <hunterliu1003@users.noreply.github.com>
2018-03-29 18:54:18 +0800

    [add] posts page

---
+++ pages/posts/index.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  .posts-page
+    p the posts
+</template>
+
+
+<style lang="stylus" scoped>
+.posts-page
+  display: flex
+  align-content: center
+  justify-content: center
+
+</style>
