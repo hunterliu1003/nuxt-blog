3e5658ba1a259afb5f7605c6bb17be7846338635
Hunter <hunterliu1003@users.noreply.github.com>
2018-03-29 18:54:26 +0800

    [add] post page

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  .single-post-page
+    section.post
+      h1 Title of the Post
+      .post-details
+        div Last updated on XXX
+        div Written by NAME
+      p Content of the post
+    section.post-feedback
+      p asdl;fkjasdlf;kdjasfl;asjk send email
+</template>
