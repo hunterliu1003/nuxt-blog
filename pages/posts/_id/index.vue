904f69b56a6aa5a865a242bb675d2d19fd12a699
Hunter <hunterliu1003@users.noreply.github.com>
2018-03-30 18:29:13 +0800

    [mod] init single post page

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,11 +1,35 @@
 <template lang="pug">
-  .single-post-page
-    section.post
-      h1 Title of the Post
-      .post-details
-        div Last updated on XXX
-        div Written by NAME
-      p Content of the post
-    section.post-feedback
-      p asdl;fkjasdlf;kdjasfl;asjk send email
+  v-container
+    v-layout(tag="section")
+      v-flex(xs12)
+        h1 {{ loadedPosts.title }}
+        div
+          v-btn(
+            small
+            v-for="tag in loadedPosts.tags"
+            :key="tag"
+          ) {{ tag }}
+        div Last updated on {{ loadedPosts.lastUpdateTime }}
+        div Written by Hunter Liu
+        p {{ loadedPosts.content }}
 </template>
+
+<script>
+export default {
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: {
+          id: '1',
+          title: 'First Post (ID: ' + context.params.id + ')',
+          lastUpdateTime: '22 Mar 2018',
+          previewText: 'first Preview Text!',
+          content: 'first Preview Text!first Preview Text!first Preview Text!first Preview Text!first Preview Text!first Preview Text!first Preview Text!',
+          tags: ['tag1', 'tag2', 'tag3', 'tag4']
+        }
+      })
+    }, 1500)
+  }
+}
+</script>
+
