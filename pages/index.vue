e797af6ac03796bde4ef7672e1757070c22ac2b0
Hunter <hunterliu1003@users.noreply.github.com>
2018-03-30 18:29:35 +0800

    [mod] using asyncData

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   v-container
     v-layout.intro(tag="section")
       h1.display-1 Get the latest tech news!
-    PostList
+    PostList(:posts="loadedPosts")
 </template>
 
 <script>
@@ -10,6 +10,29 @@ import PostList from '@/components/Posts/PostList'
 export default {
   components: {
     PostList
+  },
+  asyncData (context, callback) {
+    console.log(context)
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {
+            id: '1',
+            title: 'first',
+            lastUpdateTime: '22 Mar 2018',
+            previewText: 'first Preview Text!',
+            tags: ['tag1', 'tag2', 'tag3', 'tag4']
+          },
+          {
+            id: '2',
+            title: 'second',
+            lastUpdateTime: '22 Mar 2018',
+            previewText: 'second Preview Text!',
+            tags: ['tag5', 'tag2', 'tag3', 'tag4']
+          },
+        ]
+      })
+    }, 1500)
   }
 }
 </script>
