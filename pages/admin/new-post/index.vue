4ad8c3fde41d4769a99de836fbe7084d30ad4caf
Hunter <hunterliu1003@users.noreply.github.com>
2018-03-31 17:54:38 +0800

    [mod] newPost onSubmitted

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,14 +1,23 @@
 <template lang="pug">
   v-container
-    AdminPostForm
+    AdminPostForm(@submit="onSubmitted")
 </template>
 
 <script>
+import axios from 'axios'
 import AdminPostForm from '@/components/Admin/AdminPostForm'
 export default {
   layout: 'admin',
   components: {
     AdminPostForm
+  },
+  methods: {
+    onSubmitted(postData) {
+      this.$store.dispatch('addPost', postData)
+        .then(() => {
+          this.$router.push('/admin')
+        })
+    }
   }
 }
 </script>
