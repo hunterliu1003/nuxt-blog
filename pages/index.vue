c3ca0e33f90f3234d0b3d6dfd4b7e37bd01e07cb
Hunter <hunterliu1003@users.noreply.github.com>
2018-03-31 15:28:43 +0800

    [mod] using store getters loadedPosts

--- pages/index.vue
+++ pages/index.vue
@@ -11,28 +11,10 @@ export default {
   components: {
     PostList
   },
-  asyncData (context, callback) {
-    console.log(context)
-    setTimeout(() => {
-      callback(null, {
-        loadedPosts: [
-          {
-            id: '1',
-            title: 'first',
-            lastUpdateTime: '22 Mar 2018',
-            previewText: 'first Preview Text!',
-            tags: ['tag1', 'tag2', 'tag3', 'tag4']
-          },
-          {
-            id: '2',
-            title: 'second',
-            lastUpdateTime: '22 Mar 2018',
-            previewText: 'second Preview Text!',
-            tags: ['tag5', 'tag2', 'tag3', 'tag4']
-          },
-        ]
-      })
-    }, 1500)
+  computed: {
+    loadedPosts () {
+      return this.$store.getters.loadedPosts
+    }
   }
 }
 </script>
