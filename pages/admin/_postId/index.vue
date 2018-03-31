fffc1d8adba93bda5b240b7551c37597f8fc512b
Hunter <hunterliu1003@users.noreply.github.com>
2018-03-31 17:51:42 +0800

    [mod] connect to firebase

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,22 +1,35 @@
 <template lang="pug">
   v-container
-    AdminPostForm(:post="loadedPost")
+    AdminPostForm(:post="loadedPost" @submit="onSubmitted")
 </template>
 
 <script>
+import axios from 'axios'
 import AdminPostForm from '@/components/Admin/AdminPostForm'
+
 export default {
   layout: 'admin',
   components: {
     AdminPostForm
   },
-  data () {
-    return {
-      loadedPost: {
-        title: '',
-        previewText: '',
-        content: ''
-      }
+  asyncData (context) {
+    return axios.get(
+      'https://nuxt-blog-e0f9a.firebaseio.com/posts/' +
+        context.params.postId +
+        '.json')
+      .then(res => {
+        return {
+          loadedPost: { ...res.data, id: context.params.postId }
+        }
+      })
+      .catch(e => context.error())
+  },
+  methods: {
+    onSubmitted (editedPost) {
+      this.$store.dispatch('editPost', editedPost)
+        .then(() => {
+          this.$router.push('/admin')
+        })
     }
   }
 }
