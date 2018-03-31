de2d6316aeeafe512f56987417ab9e3afc1465e9
Hunter <hunterliu1003@users.noreply.github.com>
2018-03-31 15:28:08 +0800

    [mod] postList posts prop

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -2,7 +2,7 @@
   v-container
     v-layout.intro(tag="section")
       h1.display-1 Get the latest tech news!
-    PostList
+    PostList(:posts="loadedPosts")
 </template>
 
 <script>
@@ -10,6 +10,11 @@ import PostList from '@/components/Posts/PostList'
 export default {
   components: {
     PostList
+  },
+  computed: {
+    loadedPosts () {
+      return this.$store.getters.loadedPosts
+    }
   }
 }
 </script>
