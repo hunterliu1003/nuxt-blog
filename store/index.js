2796d16e9ed652b52d5699ea505a18cb7ec6a748
Hunter <hunterliu1003@users.noreply.github.com>
2018-04-03 18:02:26 +0800

    [mod] login & logout
    import: js-cookie
    mutations: setToken, clearToken
    actions: authenticateUser, initAuth
    getters: isAuthenticated

--- store/index.js
+++ store/index.js
@@ -1,9 +1,11 @@
 import Vuex from 'vuex'
+import Cookie from 'js-cookie'
 
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      loadedPosts: []
+      loadedPosts: [],
+      token: null
     },
     mutations: {
       setPosts(state, posts) {
@@ -17,6 +19,12 @@ const createStore = () => {
           post => post.id === editedPost.id
         )
         state.loadedPosts[postIndex] = editedPost
+      },
+      setToken(state, token) {
+        state.token = token
+      },
+      clearToken(state) {
+        state.token = null
       }
     },
     actions: {
@@ -31,7 +39,7 @@ const createStore = () => {
           })
       },
       addPost(vuexContext, post) {
-        return this.$axios.$post('/posts.json', post)
+        return this.$axios.$post('/posts.json?auth=' + vuexContext.state.token, post)
           .then(data => {
             vuexContext.commit('addPost', { ...post, id: data.name })
           })
@@ -41,7 +49,7 @@ const createStore = () => {
         return this.$axios.$put(
             '/posts/' +
             editedPost.id +
-            '.json', editedPost)
+            '.json?auth=' + vuexContext.state.token, editedPost)
           .then(data => {
             vuexContext.commit('editPost', editedPost)
           })
@@ -49,11 +57,79 @@ const createStore = () => {
       },
       setPosts(vuexContext, posts) {
         vuexContext.commit('setPosts', posts)
+      },
+      authenticateUser(vuexContext, authData) {
+        let authUrl = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + process.env.fbAPIKey
+        if (!authData.isLogin) {
+          authUrl = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' + process.env.fbAPIKey
+        }
+        return this.$axios.$post(authUrl, {
+            email: authData.email,
+            password: authData.password,
+            returnSecureToken: true
+          }
+        ).then(result => {
+          vuexContext.commit('setToken', result.idToken)
+          localStorage.setItem('token', result.idToken)
+          localStorage.setItem(
+            'tokenExpiration',
+            new Date().getTime() + Number.parseInt(result.expiresIn) * 1000
+          )
+          Cookie.set('jwt', result.idToken)
+          Cookie.set(
+            'expirationDate',
+            new Date().getTime() + Number.parseInt(result.expiresIn) * 1000
+          )
+        })
+        .catch(e => console.log(e))
+      },
+      initAuth(vuexContext, req) {
+        let token
+        let expirationDate
+        if (req) {
+          if (!req.headers.cookie) {
+            return
+          }
+          const jwtCookie = req.headers.cookie
+            .split(';')
+            .find(c => c.trim().startsWith('jwt='))
+          if (!jwtCookie) {
+            return
+          }
+          token = jwtCookie.split('=')[1]
+          expirationDate = req.headers.cookie
+            .split(';')
+            .find(c => c.trim().startsWith('expirationDate='))
+            .split('=')[1]
+        } else if (process.client) {
+          token = localStorage.getItem('token')
+          expirationDate = localStorage.getItem('tokenExpiration')
+        }
+        if (new Date().getTime() > +expirationDate || !token) {
+          console.log('no token or invalid token')
+          vuexContext.dispatch('logout')
+          return
+        }
+        vuexContext.commit('setToken', token)
+      },
+      logout (vuexContext) {
+        console.log('logout')
+        vuexContext.commit('clearToken')
+        Cookie.remove('jwt')
+        Cookie.remove('expirationDate')
+        if (process.client) {
+          localStorage.removeItem('token')
+          localStorage.removeItem('tokenExpiration')
+        }
+
       }
     },
     getters: {
       loadedPosts(state) {
         return state.loadedPosts
+      },
+      isAuthenticated(state) {
+        return state.token != null
       }
     }
   })
