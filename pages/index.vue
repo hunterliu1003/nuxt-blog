f1e1c89d2c2b5b0aa14261ea8aad7652b1186baf
Hunter <hunterliu1003@users.noreply.github.com>
2018-04-03 17:59:25 +0800

    [mod] add admin link

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   v-container
     v-layout.intro(tag="section")
       h1.display-1 Get the latest tech news!
+      v-btn(@click="$router.push('/admin')") admin
     PostList(:posts="loadedPosts")
 </template>
 
