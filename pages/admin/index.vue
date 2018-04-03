34446270e6942b74fb1a0f9f59b5015f52a460a0
Hunter <hunterliu1003@users.noreply.github.com>
2018-04-03 17:58:43 +0800

    [mod] add logout button & check-auth & auth middleware

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,6 +2,8 @@
   v-container
     v-layout.intro(tag="section")
       v-btn(@click="$router.push('/admin/new-post')") Create Post
+      v-btn(@click="$router.push('/')") home
+      v-btn(@click="onLogout") logout
     v-layout.intro(tag="section")
       h1.display-1 Existing Posts
     PostList(isAdmin :posts="loadedPosts")
@@ -11,9 +13,16 @@
 <script>
 export default {
   layout: 'admin',
+  middleware: ['check-auth', 'auth'],
   computed: {
     loadedPosts () {
       return this.$store.getters.loadedPosts
+    }
+  },
+  methods: {
+    onLogout () {
+      this.$store.dispatch('logout')
+      this.$router.push('/admin/auth')
     }
   }
 }
