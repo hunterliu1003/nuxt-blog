6ca747fe6746d09bdd2edc7d2e2c09fbc28817d7
Hunter <hunterliu1003@users.noreply.github.com>
2018-04-10 19:58:47 +0800

    [mod] fontawesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,9 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+    ],
+    script: [
+      { src: 'https://use.fontawesome.com/releases/v5.0.6/js/all.js' }
     ]
   },
 
