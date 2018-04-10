89195e667586f49c117a828ed40e51b4ee52df74
Hunter <hunterliu1003@users.noreply.github.com>
2018-04-10 18:01:27 +0800

    [mod] storybook add vuetify & material icons

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,15 +2,18 @@ import { configure } from '@storybook/vue'
 import { setOptions } from '@storybook/addon-options'
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Vuetify from 'vuetify'
 // import Sortable from 'vue-sortable'
 
 Vue.use(Vuex);
+Vue.use(Vuetify);
 Vue.directive('sortable', {
   inserted: function (el, binding) {
     new Sortable(el, binding.value || {})
   }
 })
 
+import "vuetify/dist/vuetify.css"
 // require('!!style-loader!css-loader!stylus-loader!../stylus/main.styl');
 
 setOptions({
