fe235327dbbb78fe882843f38106aec765e80a65
Hunter <hunterliu1003@users.noreply.github.com>
2018-04-12 16:43:30 +0800

    [mod] sync nuxt-blog & storybook vuetify theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,10 +3,20 @@ import { setOptions } from '@storybook/addon-options'
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Vuetify from 'vuetify'
+import colors from 'vuetify/es5/util/colors'
 // import Sortable from 'vue-sortable'
-
 Vue.use(Vuex);
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#121212', // a color that is not in the material colors palette
+    accent: colors.grey.darken3,
+    secondary: colors.amber.darken3,
+    info: colors.teal.lighten1,
+    warning: colors.amber,
+    error: colors.deepOrange.accent4,
+    success: colors.green.accent3
+  }
+})
 Vue.directive('sortable', {
   inserted: function (el, binding) {
     new Sortable(el, binding.value || {})
@@ -22,10 +32,10 @@ setOptions({
   addonPanelInRight: true
 })
 
-const req = require.context('../components', true, /stories\.js$/);
+const req = require.context('../components', true, /stories\.js$/)
 
 function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
