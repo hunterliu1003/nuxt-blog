da38f30372a26124c9242633b82ab0ae8b6d617e
Hunter <hunterliu1003@users.noreply.github.com>
2018-04-12 16:42:32 +0800

    [add] storybook webpack resolve.alias & addon-storysource

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+const genDefaultConfig = require('@storybook/vue/dist/server/config/defaults/webpack.config.js');
+
+module.exports = (baseConfig, env) => {
+  const config = genDefaultConfig(baseConfig, env);
+  config.resolve.alias['@'] = path.dirname(path.resolve(__dirname))
+  // console.log(config)
+  config.module.rules.push({
+    test: /\.stories\.jsx?$/,
+    loaders: [require.resolve('@storybook/addon-storysource/loader')],
+    enforce: 'pre'
+  })
+  return config
+}
