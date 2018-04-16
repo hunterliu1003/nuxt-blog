99ba21431904524d64139f7f63c7b9a20459a735
Hunter <hunterliu1003@users.noreply.github.com>
2018-04-16 16:25:24 +0800

    [mod] add global component PostsPostMarkdown

--- plugins/core-components.js
+++ plugins/core-components.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 
 import PostsPostList from '@/components/PostsPostList'
 import TheInputTags from '@/components/TheInputTags'
+import PostsPostMarkdown from '@/components/PostsPostMarkdown'
 
 Vue.component('PostsPostList', PostsPostList)
 Vue.component('TheInputTags', TheInputTags)
+Vue.component('PostsPostMarkdown', PostsPostMarkdown)
