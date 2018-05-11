bdeea46c9b4839684e6afcee0322e8a3517ea137
Hunter <hunterliu1003@users.noreply.github.com>
2018-05-11 17:11:37 +0800

    [mod] new theme color

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -10,6 +10,8 @@ Vue.use(Vuetify, {
     info: colors.teal.lighten1,
     warning: colors.amber,
     error: colors.deepOrange.accent4,
-    success: colors.green.accent3
+    success: colors.green.accent3,
+    'ht-post-preview': '#3B8070',
+    'ht-main-bgc': '#303030'
   }
 })
